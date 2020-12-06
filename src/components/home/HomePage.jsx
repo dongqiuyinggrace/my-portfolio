c44ad6d2dd49c2069d284e60a90a8b638d812f99
Grace <dongqiuyinggrace@gmail.com>
2020-12-06 22:34:08 +1100

    firstCommit

---
+++ src/components/home/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './HomePage.css'
+
+const HomePage = () => {
+    return (
+        <div id="hero">
+            <h1>I am Grace Dong</h1>
+            <h3>I am a full stack developer</h3>
+            <p>More Information</p>
+        </div>
+    )
+}
+
+export default HomePage
